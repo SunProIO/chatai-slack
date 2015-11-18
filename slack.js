2e5b92e801b10de8217e2ec5325d3b75603733b1
Koki Takahashi <hakatasiloving@gmail.com>
2015-11-18 16:47:02 +0900

    Split slack things to slack.js

---
+++ slack.js
@@ -0,0 +1,22 @@
+/***** Slack things *****/
+
+const Slack = require('slack-client');
+
+const slack = new Slack(secret.token, true, true);
+
+const channels = {};
+
+slack.on('open', function () {
+	Object.keys(slack.channels).forEach(function (id) {
+		channels[slack.channels[id].name] = slack.channels[id];
+	});
+});
+
+slack.on('error', function (error) {
+	console.error(error);
+});
+
+slack.login();
+
+
+module.exports = slack;
