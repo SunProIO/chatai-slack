35320bce54e867e9b6450dc861a62185d3f546b8
Koki Takahashi <hakatasiloving@gmail.com>
2015-11-19 14:45:28 +0900

    Add env.example.cmd

---
+++ env.example.cmd
@@ -0,0 +1,6 @@
+set REDIS_URL=redis://localhost:6379
+set GOOGLE_CLIENT_SECRETS={"installed":"hogehoge".....}
+set SECRET_JSON_ID=99-XXXXXXXXXXXXXXXXXXXXXXXXX
+set SLACK_TOKEN=xxxx-9999999999-xxxxxxxxxxxxxxxxxxxxxxxx
+set CHATAI_ENV=development
+set CHATAI_TYPE=day
